40c0e1d3a6ab31aa0eb626fa89af883a0c23e8d8
Pipe1995Garzon <47527537+Pipe1995Garzon@users.noreply.github.com>
2022-05-13 11:04:46 -0500

    cracion App Dogs

---
+++ App.js
@@ -0,0 +1,6 @@
+import { StatusBar } from 'expo-status-bar';
+import Main from '.src/components/Main.jsx';
+
+export default function App() {
+  return <Main/>
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Constants from 'expo-constants';
+import {Text, View} from 'react-native';
+
+const Main = ()=>{
+  return(
+    <View style={{marginTop: Constants.statusBarHeight, flexGrow: 1}}>
+      <Text>Para nosotros es un  gusto darte la bienvenida a Dogs.</Text>
+    </View>
+  )
+}
